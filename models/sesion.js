29ec3d1d098e7e1447283b28f1be124841ba4184
adbias manases palacios barahona <apalacio@alumnos.inf.utfsm.cl>
2017-09-18 23:17:39 -0300

    Added session model, and modified user model

---
+++ models/sesion.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var Sesion = sequelize.define("Sesion", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        titulo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        escenario: {
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+        link: {
+            type: DataTypes.STRING,
+            allowNull: false
+
+        }}, {
+            classMethods: {
+                associate: function(models) {
+                    Sesion.belongsTo(models.Usuario, {
+                        foreignKey: {
+                            allowNull: false
+                        }
+                    });
+                }
+            }
+        });
+    return Sesion;
+};
--- models/usuario.js
+++ models/usuario.js
@@ -2,14 +2,25 @@
 
 module.exports = function(sequelize, DataTypes) {
     var Usuario = sequelize.define("Usuario", {
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        email: DataTypes.STRING
-    }, {
-        classMethods: {
-            associate: function(models) {
-                Usuario.hasMany(models.Rol)
-            }
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+
         }
     });
     return Usuario;
