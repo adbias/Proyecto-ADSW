85780df31fb35722bf871b6db121ba4479ff8c31
camilo maldonado <cmaldona@camilos-MacBook-Air.local>
2017-09-19 22:35:55 -0300

    changes routes and header bar

--- router/routes.js
+++ router/routes.js
@@ -31,49 +31,12 @@ app.get('/chat2',function(req,res){
     res.render('chat2.html', {title: 'Chat'});
 });
 
-app.get('/verUsuario',function(req,res){
-    res.redirect('/api/usuarios')
-});
-
 app.get('/crearUsuario',function(req,res){
-    res.render('CrearUsuario.html', {title: 'Registro de Usuario'});
+    res.render('CrearUsuario.html', {title: 'Registro de Usuario', session: req.session});
 });
 
 app.get('/createSesion',function(req,res){
         res.render('CreateSesion.html', {title: 'Crear Sesion'});
-});
-
-
-app.get('/chat/12345', function (req,res,next) {
-    try {
-        return models.Chat.findAll().then(function (chat) {
-            res.json(chat);
-        })
-    }
-    catch(ex){
-        console.error("Internal error:"+ex);
-        return next(ex);
-    }
-
-});
-app.post('/chat/12345',function (req, res, next) {
-    console.log("Access through post");
-    try {
-        var client = chat.client(room);
-        console.log(req, "chat");
-        client.identify({ nick:req.username });
-        client.once('ready', function() {
-            client.write(req.msg);
-        });
-        models.Chat.create({
-            username: req.username,
-            msg: req.msg
-        });
-    }
-    catch(ex){
-        console.error("Internal error:"+ex);
-        return next(ex);
-    }
 });
 
 app.get('/logout', function(req, res) {
